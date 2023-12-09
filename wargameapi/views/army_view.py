13c7ff0d90c3273c9b1138ffccbd02362c689e1e
Justin Miller <jrmill94@icloud.com>
2023-12-09 04:32:25 -0600

    wip

--- wargameapi/views/army_view.py
+++ wargameapi/views/army_view.py
@@ -20,8 +20,23 @@ class ArmyView(ViewSet):
         serialized = ArmySerializer(army, many=False)
         return Response(serialized.data, status=status.HTTP_200_OK)
     
-    # def create(self, request):
+    def create(self, request):
+        name = request.data.get('name')
+        image_url = request.data.get('image_url')
+        points = request.data.get('points')
+        description = request.data.get('description')
+        category_id = request.data.get('category')
 
+        army = Army.objects.create(
+            user=request.user.wargameuser,
+            name=name,
+            image_url=image_url,
+            points=points,
+            description=description,
+            category_id=category_id,
+        )
+
+        
 
     # def destroy(self, request, pk=None):
 
