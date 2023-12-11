8661a17806cfef701030729618a6c9801f63f767
Justin Miller <jrmill94@icloud.com>
2023-12-11 10:59:40 -0600

    army view completed

--- wargameapi/views/army_view.py
+++ wargameapi/views/army_view.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers, status
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
+from django.core.exceptions import PermissionDenied
 from wargameapi.models import Army, WargameUser, Category
 
 class ArmySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Army
+        fields = ('id', 'name', 'image_url', 'points', 'description', 'category', 'user',)
+
+class ArmyUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Army
         fields = ('id', 'name', 'image_url', 'points', 'description', 'category', 'user',)
@@ -36,8 +42,38 @@ class ArmyView(ViewSet):
             category_id=category_id,
         )
 
+        serializer = ArmySerializer(army, context={'request': request})
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+    def destroy(self, request, pk=None):
+        try:
+            army = Army.objects.get(pk=pk)
+
+            if request.user.wargameuser.id != army.user_id:
+                raise PermissionDenied("You do not have permission to delete this army.")
+            
+            army.delete()
+
+            return Response(status=status.HTTP_204_NO_CONTENT)
         
-
-    # def destroy(self, request, pk=None):
-
-
+        except Army.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+    def update(self, request, pk=None):
+        try:
+            army = Army.objects.get(pk=pk)
+            serializer = ArmyUpdateSerializer(army, data=request.data, context={'request': request})
+            if serializer.is_valid():
+                army.user = serializer.validated_data['user']
+                army.name = serializer.validated_data['name']
+                army.image_url = serializer.validated_data['image_url']
+                army.points = serializer.validated_data['points']
+                army.description = serializer.validated_data['description']
+                army.category_id = serializer.validated_data['category']
+                army.save()
+                serializer = ArmyUpdateSerializer(army, context={'request': request})
+                return Response(None, status.HTTP_204_NO_CONTENT)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Army.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
